4607091654df36e8242b50e9efd333b6da32d116
ZIYAN WANG <zw7g20@soton.ac.uk>
2023-03-14 15:45:12 +0000

    Modified for CRAN

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -23,7 +23,7 @@ static const R_CallMethodDef CallEntries[] = {
     {NULL, NULL, 0}
 };
 
-RcppExport void R_init_PlatFormDesignTimeTrend(DllInfo *dll) {
+RcppExport void R_init_BayesianPlatformDesignTimeTrend(DllInfo *dll) {
     R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
     R_useDynamicSymbols(dll, FALSE);
 }
